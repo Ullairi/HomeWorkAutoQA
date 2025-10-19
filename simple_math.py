0010b2a1668bc5a802ee5c8a568a7895b3f0e664
Ullairi <maks.tih2005@gmail.com>
2025-10-19 15:34:04 +0100

    Created SimpleMath class with square() and cube() methods

---
+++ simple_math.py
@@ -0,0 +1,10 @@
+class SimpleMath:
+    """Class with simple math operations"""
+
+    def square(self, x):
+        """Return square of a number"""
+        return x * x
+
+    def cube(self, x):
+        """Return cube of a number"""
+        return x * x * x
