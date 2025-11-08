3da58388c2136db96252be127cd1fed2d4b61443
Ullairi <maks.tih2005@gmail.com>
2025-11-08 16:32:21 +0100

    Tests now use CSS_SELECTOR instead of XPATH

--- selenhhw5.py
+++ selenhhw5.py
@@ -42,21 +42,14 @@ def test_reviews_link_displayed(driver):
     assert reviews.is_displayed(), "Ссылка 'Отзывы' не найдена"
 
 def test_buttons_ru_de(driver):
+    button_de = driver.find_element(By.CSS_SELECTOR, 'a.tn-atom[href="/"]')
+    driver.execute_script("arguments[0].click();", button_de)
     time.sleep(3)
-    button_de = driver.find_elements(By.XPATH, '//a[contains(@href,"/de")]')
-    button_ru = driver.find_elements(By.XPATH, '//a[contains(@href,"/ru")]')
+    assert "itcareerhub.de" in driver.current_url, "После клика на de язык не переключился обратно"
 
-    assert len(button_de) > 0, "de кнопка не найдена"
-    assert len(button_ru) > 0, "ru кнопка не найдена"
-
-
-def test_phone_button(driver):
-    driver.get("https://itcareerhub.de/ru/contact-us")
+    button_ru = driver.find_element(By.CSS_SELECTOR, 'a.tn-atom[href="/ru"]')
+    driver.execute_script("arguments[0].click();", button_ru)
     time.sleep(3)
 
-    phone_icon = driver.find_element(By.XPATH, '//a[contains(@href,"#popup:form-tr3")]')
-    driver.execute_script("arguments[0].click();", phone_icon)
-    time.sleep(2)
-
-    text = driver.find_element(By.XPATH, '//div[@class="tn-atom" and contains(text(),"Если вы не дозвонились")]')
-    assert text.is_displayed(), "После клика текстт не отображается"
+    assert "/ru" in driver.current_url, "после клика на ru язык не переключился обратно"
+    assert "Программы" in driver.page_source, "Страница не вернулась обрано на русский язык"
